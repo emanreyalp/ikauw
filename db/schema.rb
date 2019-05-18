5fd7955134e593c7e3874715f2df7a1cffbbe762
Gergely Dömök <emanreyalp@users.noreply.github.com>
2019-05-18 11:50:32 +0200

    Generate Content
     - rails g scaffold Content type:string title:string plot:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_173611) do
+ActiveRecord::Schema.define(version: 2019_05_18_093804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "type"
+    t.string "title"
+    t.string "plot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
---
+++ spec/factories/contents.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :content do
+    type { "" }
+    title { "MyString" }
+    plot { "MyString" }
+  end
+end
