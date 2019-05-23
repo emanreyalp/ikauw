f7cb7832cec853283d7462eb739c07f4ac628446
Gergely Dömök <emanreyalp@users.noreply.github.com>
2019-05-23 15:00:42 +0200

    Couldn't purchase an episode

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,10 +4,17 @@ class Purchase < ApplicationRecord
   belongs_to :purchase_option
 
   validates_uniqueness_of :user_id, scope: [:content_id, :purchase_option_id]
+  validate :not_episode
 
   scope :active_in_time, lambda { |activation_period_time|
     where('from_date >= ?', Time.now - activation_period_time) }
   scope :active, lambda { active_in_time(ACTIVATION_PERIOD_TIME) }
 
   ACTIVATION_PERIOD_TIME = 2.day
+
+  private
+
+  def not_episode
+    errors.add(:content, "can't be Episode") if content.class == Episode
+  end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "can't be beloging to an Episode" do
+    purchase = build(:purchase, content: create(:episode))
+
+    expect(purchase).not_to be_valid
+  end
 end
