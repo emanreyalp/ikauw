c3b7cdce89affa8932ecd85d43db4e7772a1dc48
emanreyalp <emanreyalp@users.noreply.github.com>
2020-02-08 18:09:18 +0100

    Test that can't create pure content.
    Has to be Movie, Season or Episode

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should raise error when initializing' do
+    expect { build(:content) }.to raise_error
+  end
 end
