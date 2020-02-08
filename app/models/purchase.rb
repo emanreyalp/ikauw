71884d312c494ff1c8c743ce9aecbd657640cda1
emanreyalp <emanreyalp@users.noreply.github.com>
2020-02-08 23:05:23 +0100

    Reorder macro-style methods in purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,11 +1,4 @@
 class Purchase < ApplicationRecord
-  belongs_to :user
-  belongs_to :content
-  belongs_to :purchase_option
-
-  validates_uniqueness_of :user_id, scope: [:content_id, :purchase_option_id]
-  validate :not_episode
-
   scope :active_in_time, lambda { |activation_period_time|
     where('from_date >= ?', Time.now - activation_period_time) }
   scope :active, lambda { active_in_time(ACTIVATION_PERIOD_TIME) }
@@ -14,6 +7,13 @@ class Purchase < ApplicationRecord
   scope :ordered_by_expiration, lambda { order(:created_at) }
 
   ACTIVATION_PERIOD_TIME = 2.day
+
+  belongs_to :user
+  belongs_to :content
+  belongs_to :purchase_option
+
+  validates_uniqueness_of :user_id, scope: [:content_id, :purchase_option_id]
+  validate :not_episode
 
   private
 
