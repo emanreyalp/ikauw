1e32bba72ab0b4fa0834e1395da0a332ce876781
emanreyalp <emanreyalp@users.noreply.github.com>
2020-02-10 11:55:48 +0100

    Delete unused users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :movies, only: :index
   resources :contents, only: :index do
     resources :purchases, only: :create
-    # resources :purchases, only: [:create, :show]
   end
   resources :library, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
